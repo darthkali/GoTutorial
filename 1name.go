6e999b68d516a6c46d874460333f69ad306f5bf2
dannysteinbrecher <danny.steinbrecher@outlook.com>
2025-07-03 08:51:00 +0200

    Initial commit

---
+++ 1name.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	name := "Danny"
+	age := 35
+
+	fmt.Println("Hallo, ich bin", name, "und", age, "Jahre alt.")
+	fmt.Printf("Hallo, ich bin %s und %d Jahre alt.\n", name, age)
+}
